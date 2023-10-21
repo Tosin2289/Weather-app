05be903ee15ff64e051778f68865073750857a47
Tosin2289 <ayoolatosin00@gmail.com>
2023-10-21 01:57:52 +0100

    serch page redesign

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,4 +1,8 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:weather_app/pages/weather_Page.dart';
 
 class SearchPage extends StatelessWidget {
@@ -7,88 +11,212 @@ class SearchPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey.shade200,
-      body: Center(
-        child: Column(
-          children: [
-            const Expanded(
-                child: SizedBox(
-                    child: Center(
-              child: Text(
-                "Weather in your city",
-                style: TextStyle(
-                  fontSize: 35,
-                  fontWeight: FontWeight.w600,
+      backgroundColor: Colors.black,
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        systemOverlayStyle:
+            const SystemUiOverlayStyle(statusBarBrightness: Brightness.dark),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(25),
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          child: Stack(
+            children: [
+              Align(
+                alignment: const AlignmentDirectional(6, -0.3),
+                child: Container(
+                  height: 300,
+                  width: 300,
+                  decoration: const BoxDecoration(
+                      shape: BoxShape.circle, color: Colors.deepPurple),
                 ),
               ),
-            ))),
-            Expanded(
+              Align(
+                alignment: const AlignmentDirectional(-8, -0.3),
                 child: Container(
-              decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(50),
-                      topRight: Radius.circular(50))),
-              child: Center(
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
+                  height: 300,
+                  width: 300,
+                  decoration: const BoxDecoration(
+                      shape: BoxShape.circle, color: Colors.deepPurple),
+                ),
+              ),
+              Align(
+                alignment: const AlignmentDirectional(0, -1.2),
+                child: Container(
+                  height: 300,
+                  width: 600,
+                  decoration: const BoxDecoration(color: Colors.orange),
+                ),
+              ),
+              Align(
+                alignment: const AlignmentDirectional(0, 1),
+                child: Container(
+                  height: 300,
+                  width: 600,
+                  decoration: const BoxDecoration(color: Colors.orange),
+                ),
+              ),
+              Align(
+                alignment: const AlignmentDirectional(6, -0.3),
+                child: Container(
+                  height: 300,
+                  width: 300,
+                  decoration: const BoxDecoration(
+                      shape: BoxShape.circle, color: Colors.deepPurple),
+                ),
+              ),
+              Align(
+                alignment: const AlignmentDirectional(-8, -0.3),
+                child: Container(
+                  height: 300,
+                  width: 300,
+                  decoration: const BoxDecoration(
+                      shape: BoxShape.circle, color: Colors.deepPurple),
+                ),
+              ),
+              BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 100.0, sigmaY: 100),
+                  child: Container(
+                    decoration: const BoxDecoration(color: Colors.transparent),
+                  )),
+              Align(
+                alignment: Alignment.center,
+                child: Container(
+                  width: double.infinity,
+                  height: 400,
+                  decoration: BoxDecoration(
+                    color: Colors.transparent,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Stack(
                     children: [
+                      BackdropFilter(
+                        filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
+                        child: Container(),
+                      ),
                       Container(
                         decoration: BoxDecoration(
-                            color: Colors.grey.shade200,
-                            borderRadius: BorderRadius.circular(10)),
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 12.0, vertical: 6),
-                          child: TextField(
-                            controller: weatherController,
-                            decoration: const InputDecoration(
-                                border: InputBorder.none,
-                                hintText: 'Enter your city',
-                                hintStyle: TextStyle(
-                                    fontSize: 16, color: Colors.grey)),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 30,
-                      ),
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.push(context, MaterialPageRoute(
-                            builder: (context) {
-                              return HomePage(
-                                locations: weatherController.text,
-                              );
-                            },
-                          ));
-                        },
-                        child: Container(
-                          height: 50,
-                          width: double.infinity,
-                          decoration: BoxDecoration(
-                            color: Colors.black,
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: const Center(
-                            child: Text(
-                              "Search",
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold),
+                            borderRadius: BorderRadius.circular(20),
+                            border: Border.all(
+                              color: Colors.white.withOpacity(0.13),
                             ),
-                          ),
-                        ),
+                            gradient: LinearGradient(
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight,
+                              colors: [
+                                Colors.white.withOpacity(0.15),
+                                Colors.white.withOpacity(0.5),
+                              ],
+                            )),
                       ),
+                      Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Text(
+                              "Weather in your city",
+                              style: TextStyle(
+                                  fontSize: 30,
+                                  color: Colors.white70.withOpacity(0.4),
+                                  fontWeight: FontWeight.w600),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(12.0),
+                              child: Column(
+                                children: [
+                                  TextField(
+                                    controller: weatherController,
+                                    style:
+                                        const TextStyle(color: Colors.white54),
+                                    decoration: InputDecoration(
+                                      filled: true,
+                                      hintText: 'Enter city',
+                                      hintStyle: const TextStyle(
+                                          color: Colors.white54),
+                                      fillColor:
+                                          Colors.white70.withOpacity(0.2),
+                                      focusedBorder: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(12),
+                                          borderSide: BorderSide(
+                                            color:
+                                                Colors.white.withOpacity(0.5),
+                                          )),
+                                      enabledBorder: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(12),
+                                          borderSide: BorderSide(
+                                            color:
+                                                Colors.white.withOpacity(0.5),
+                                          )),
+                                      disabledBorder: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(12),
+                                          borderSide: BorderSide(
+                                            color:
+                                                Colors.white.withOpacity(0.13),
+                                          )),
+                                      border: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(12),
+                                          borderSide: BorderSide(
+                                            color:
+                                                Colors.white.withOpacity(0.13),
+                                          )),
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    height: 20,
+                                  ),
+                                  SizedBox(
+                                    height: 50,
+                                    width: double.infinity,
+                                    child: ElevatedButton(
+                                        style: ElevatedButton.styleFrom(
+                                            elevation: 0,
+                                            backgroundColor:
+                                                Colors.white.withOpacity(0.3),
+                                            shape: RoundedRectangleBorder(
+                                                borderRadius:
+                                                    BorderRadius.circular(12))),
+                                        onPressed: () {
+                                          Navigator.push(context,
+                                              MaterialPageRoute(
+                                            builder: (context) {
+                                              return HomePage(
+                                                  locations:
+                                                      weatherController.text);
+                                            },
+                                          ));
+                                        },
+                                        child: const Text(
+                                          "Search",
+                                          style: TextStyle(
+                                            fontSize: 16,
+                                          ),
+                                        )),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+                          .animate(delay: 100.ms)
+                          .fadeIn(duration: 800.ms)
+                          .moveY(begin: 15, end: 0),
                     ],
                   ),
-                ),
+                )
+                    .animate(delay: 100.ms)
+                    .fadeIn(duration: 1500.ms)
+                    .moveY(begin: 15, end: 0),
               ),
-            )),
-          ],
+            ],
+          ),
         ),
       ),
     );
